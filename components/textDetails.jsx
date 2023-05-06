2b27ecb1b6fd6c70bca0721b972b719753da24e5
Alex Masinde <daktari2033@gmail.com>
2023-05-06 23:13:33 +0300

    some changes

--- components/textDetails.jsx
+++ components/textDetails.jsx
@@ -5,17 +5,17 @@ export default function TextDetails() {
     <div className="w-full lg:w-1/2 max-w-md mx-auto lg:mx-0 xs:px-[5%] sm:px-[0%] flex flex-col items-center">
       <div className="bg-[#fff] rounded-[8px] p-5 h-[100%]">
       <ul className="space-y-4">
-            <li class="flex items-center">
-              <svg class="h-6 w-6 flex-none fill-[#fff] stroke-[#514887] stroke-2" stroke-linecap="round" stroke-linejoin="round">
+            <li className="flex items-center">
+              <svg className="h-6 w-6 flex-none fill-[#fff] stroke-[#514887] stroke-2" stroke-linecap="round" stroke-linejoin="round">
                 <circle cx="12" cy="12" r="11" />
                 <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
               </svg>
               <p className="ml-4">
-                Fin in your personal details accurately then click register
+                Fill in your personal details accurately then click register
               </p>
             </li>
-            <li class="flex items-center">
-              <svg class="h-6 w-6 flex-none fill-[#fff] stroke-[#514887] stroke-2" stroke-linecap="round" stroke-linejoin="round">
+            <li className="flex items-center">
+              <svg className="h-6 w-6 flex-none fill-[#fff] stroke-[#514887] stroke-2" stroke-linecap="round" stroke-linejoin="round">
                 <circle cx="12" cy="12" r="11" />
                 <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
               </svg>
@@ -23,8 +23,8 @@ export default function TextDetails() {
                 Make your payment for the conference to paybill number <strong className="text-sm font-bold text-gray-900">675252</strong>
               </p>
           </li>
-          <li class="flex items-center">
-              <svg class="h-6 w-6 flex-none fill-[#fff] stroke-[#514887] stroke-2" stroke-linecap="round" stroke-linejoin="round">
+          <li className="flex items-center">
+              <svg className="h-6 w-6 flex-none fill-[#fff] stroke-[#514887] stroke-2" stroke-linecap="round" stroke-linejoin="round">
                 <circle cx="12" cy="12" r="11" />
                 <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
               </svg>
@@ -32,8 +32,8 @@ export default function TextDetails() {
                 For account number, use your <strong className="text-sm font-bold text-gray-900">name</strong>
               </p>
             </li>
-          <li class="flex items-center">
-              <svg class="h-6 w-6 flex-none fill-[#fff] stroke-[#514887] stroke-2" stroke-linecap="round" stroke-linejoin="round">
+          <li className="flex items-center">
+              <svg className="h-6 w-6 flex-none fill-[#fff] stroke-[#514887] stroke-2" stroke-linecap="round" stroke-linejoin="round">
                 <circle cx="12" cy="12" r="11" />
                 <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
               </svg>
@@ -41,8 +41,8 @@ export default function TextDetails() {
                 Enter the transaction code <strong className="text-sm font-bold text-gray-900">(e.g. RDK7NQ9AMH)</strong> that you receive to complete registration
               </p>
             </li>
-          <li class="flex items-center">
-              <svg class="h-6 w-6 flex-none fill-[#fff] stroke-[#514887] stroke-2" stroke-linecap="round" stroke-linejoin="round">
+          <li className="flex items-center">
+              <svg className="h-6 w-6 flex-none fill-[#fff] stroke-[#514887] stroke-2" stroke-linecap="round" stroke-linejoin="round">
                 <circle cx="12" cy="12" r="11" />
                 <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
               </svg>
