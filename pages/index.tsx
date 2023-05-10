9082572e527a924e49ead62bc4b1071e0cf20019
Kelvin Kitika <lukkitt@live.co.uk>
2023-05-11 00:19:05 +0300

    add titles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Image from "next/image";
+import Head from "next/head";
 import logo from "../public/images/kssha_logo.png";
 import Delegates from "@/components/delegates";
 import Exhibitors from "@/components/exhibitors";
@@ -7,46 +8,53 @@ import Exhibitors from "@/components/exhibitors";
 export default function Home() {
   const [showDelegates, setShowDelegates] = useState(true);
   return (
-    <main className="lg:bg-blue-100 min-h-screen pat relative lg:pt-[10px]">
-      <div className="pb-[80px] lg:w-[60%] mx-auto z-10 lg:rounded-[8px]">
-        <div className="w-full mx-auto flex flex-col items-center bg-white rounded-[6px]  py-[16px]">
-          <div className="max-w-[280px] flex flex-row items-center">
-            <Image src={logo} alt="KSSHA logo" />
-            <h1 className="ml-[10px] text-lg leading-5">
-              Kenya Secondary Schools Heads Association
-            </h1>
+    <>
+      <Head>
+        <title>KSSHA Registration Portal</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <main className="lg:bg-blue-100 min-h-screen pat relative lg:pt-[10px]">
+        <div className="pb-[80px] lg:w-[60%] mx-auto z-10 lg:rounded-[8px]">
+          <div className="w-full mx-auto flex flex-col items-center bg-white rounded-[6px]  py-[16px]">
+            <div className="max-w-[280px] flex flex-row items-center">
+              <Image src={logo} alt="KSSHA logo" />
+              <h1 className="ml-[10px] text-lg leading-5">
+                Kenya Secondary Schools Heads Association
+              </h1>
+            </div>
+            <div className="mt-[12px] flex flex-col items-center p-5">
+              <h1 className="text-3xl font-extrabold ">
+                KSSHA 46TH ANNUAL NATIONAL CONFERENCE OF PRINCIPALS AND
+                EXHIBITION
+              </h1>
+              <p className="text-lg opacity-60">
+                26th to 30th June 2023 at the Sheikh Zayed Hall in Mombasa.
+              </p>
+            </div>
           </div>
-          <div className="mt-[12px] flex flex-col items-center p-5">
-            <h1 className="text-3xl font-extrabold ">
-              KSSHA 46TH ANNUAL NATIONAL CONFERENCE OF PRINCIPALS AND EXHIBITION
-            </h1>
-            <p className="text-lg opacity-60">
-              26th to 30th June 2023 at the Sheikh Zayed Hall in Mombasa.
-            </p>
+          <div className="flex flex-row justify-between bg-white mt-[10px] rounded-bl-none rounded-br-none border-b border-b-gray-200 rounded-tl-[8px] rounded-tr-[8px]">
+            <div
+              onClick={() => setShowDelegates(true)}
+              className={`w-1/2 flex flex-col items-center py-[10px] rounded-tl-[8px] cursor-pointer ${
+                showDelegates ? "bg-[#514887] text-white" : ""
+              }`}
+            >
+              <p>Delegate Registration</p>
+            </div>
+            <div
+              onClick={() => setShowDelegates(false)}
+              className={`w-1/2 flex flex-col items-center py-[10px] rounded-tr-[8px] cursor-pointer ${
+                showDelegates ? "" : " bg-[#514887] text-white"
+              }`}
+            >
+              <p>Exhibitor Registration</p>
+            </div>
+          </div>
+          <div className="flex flex-col lg:flex-row w-full mx-auto justify-between bg-white rounded-[8px] p-5 shadow-lg shadow-[rgba(81, 72, 135, 1)] rounded-tl-none rounded-tr-none">
+            {showDelegates ? <Delegates /> : <Exhibitors />}
           </div>
         </div>
-        <div className="flex flex-row justify-between bg-white mt-[10px] rounded-bl-none rounded-br-none border-b border-b-gray-200 rounded-tl-[8px] rounded-tr-[8px]">
-          <div
-            onClick={() => setShowDelegates(true)}
-            className={`w-1/2 flex flex-col items-center py-[10px] rounded-tl-[8px] cursor-pointer ${
-              showDelegates ? "bg-[#514887] text-white" : ""
-            }`}
-          >
-            <p>Delegate Registration</p>
-          </div>
-          <div
-            onClick={() => setShowDelegates(false)}
-            className={`w-1/2 flex flex-col items-center py-[10px] rounded-tr-[8px] cursor-pointer ${
-              showDelegates ? "" : " bg-[#514887] text-white"
-            }`}
-          >
-            <p>Exhibitor Registration</p>
-          </div>
-        </div>
-        <div className="flex flex-col lg:flex-row w-full mx-auto justify-between bg-white rounded-[8px] p-5 shadow-lg shadow-[rgba(81, 72, 135, 1)] rounded-tl-none rounded-tr-none">
-          {showDelegates ? <Delegates /> : <Exhibitors />}
-        </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
