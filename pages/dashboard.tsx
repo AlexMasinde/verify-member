72afa3d8295c17ab9f929839904f2d39545588e4
Kelvin Kitika <lukkitt@live.co.uk>
2023-05-11 11:44:22 +0300

    fixes

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -3,11 +3,18 @@ import ExhibitorsData from "@/components/exhibitorsData";
 import ExhibitorContextProvider, {
   useExhibitorContext,
 } from "@/contexts/exhibitorContext";
+import Head from "next/head";
 
 export default function Dashboard() {
   return (
-    <ExhibitorContextProvider>
-      <ExhibitorsContainer />
-    </ExhibitorContextProvider>
+    <>
+      <Head>
+        <title>KSSHA Dashboard</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <ExhibitorContextProvider>
+        <ExhibitorsContainer />
+      </ExhibitorContextProvider>
+    </>
   );
 }
