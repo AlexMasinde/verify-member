3753cd45cfc1a467d364f9297384d66c121b2f1a
Alex Masinde <daktari2033@gmail.com>
2023-09-28 13:11:29 +0300

    initial commit

--- pages/checkAttendance.tsx
+++ pages/checkAttendance.tsx
@@ -35,10 +35,6 @@ export default function CheckAttendance() {
   const [isMobile] = useMediaQuery("(max-width: 768px)");
   const [loading, setLoading] = useState(false);
   const [idNumber, setIdNumber] = useState("");
-  const [statistics, setStatistics] = useState({
-    totalMembers: 0,
-    totalCheckins: 0,
-  });
   const [userDetails, setUserDetails] = useState<null | UserStatusDetails>(
     null
   );
@@ -50,25 +46,6 @@ export default function CheckAttendance() {
   const [error, setError] = useState("");
 
   const toast = useToast();
-
-  useEffect(() => {
-    async function fetchStats() {
-      console.log("Effect ran");
-      try {
-        if (!token) return;
-        const url = "/event/stats";
-        const response = await getDataWithRetries(url, token);
-        const data = response.data;
-        setStatistics({
-          totalCheckins: data.totalCheckins,
-          totalMembers: data.totalMembers,
-        });
-      } catch (err) {
-        console.log(err);
-      }
-    }
-    fetchStats();
-  }, [token]);
 
   function handleId(event: React.ChangeEvent<HTMLInputElement>) {
     if (error) setError("");
@@ -141,18 +118,6 @@ export default function CheckAttendance() {
   return (
     <div className="flex flex-col items-center justify-center">
       <Container maxW="400px">
-        <div className="border border-grey-300 rounded-md p-2 mb-2">
-          <Text fontSize="md">Total Members</Text>
-          <Text fontSize="lg" fontWeight="semibold">
-            {statistics.totalMembers}
-          </Text>
-        </div>
-        <div className="border border-grey-300 rounded-md p-2 mb-2">
-          <Text fontSize="md">Total Check-ins</Text>
-          <Text fontSize="lg" fontWeight="semibold">
-            {statistics.totalCheckins}
-          </Text>
-        </div>
         <TextContainer>
           <Link href="/">
             <Image src={logo} alt="UDA Logo" width={42} />
@@ -181,161 +146,14 @@ export default function CheckAttendance() {
         >
           {userDetails ? "Clear" : "Check Status"}
         </Button>
-        {userDetails ? (
-          <UserDetails
-            userDetails={userDetails}
-            setUserDetails={setUserDetails}
-            setSerialNumber={setIdNumber}
-          />
-        ) : null}
+        {userDetails ? <UserDetails userDetails={userDetails} /> : null}
       </Container>
     </div>
   );
 }
 
-function UserDetails({
-  userDetails,
-  setUserDetails,
-  setSerialNumber,
-}: {
-  userDetails: UserStatusDetails;
-  setUserDetails: (value: UserStatusDetails | null) => void;
-  setSerialNumber: (value: string) => void;
-}) {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
-  const [message, setMessage] = useState("");
-
-  const contextData = useExhibitorContext();
-
-  const token = contextData.user?.authToken;
-
-  const toast = useToast();
-
-  async function checkIn() {
-    if (userDetails.arrears) {
-      toast({
-        title: "Arrears",
-        description: `Unable to check-in! Please clear your arrears of KES ${userDetails.arrears}`,
-        status: "error",
-        duration: 4000,
-        isClosable: true,
-      });
-      return;
-    }
-
-    try {
-      setLoading(true);
-      if (!token) {
-        toast({
-          title: "Error",
-          description: "Must be authenticated to fetch user data",
-          status: "error",
-          duration: 3000,
-          isClosable: true,
-        });
-        return;
-      }
-      const url = `/event/check-in/${userDetails.idNumber}`;
-      const response = await putDataWithRetries(url, token);
-      setMessage("User Checked in Successfully");
-      console.log(response);
-      setLoading(false);
-      setMessage("");
-      toast({
-        title: "Checked In.",
-        description: `Successfully checked in ${userDetails.fullName}`,
-        status: "success",
-        duration: 3000,
-        isClosable: true,
-      });
-      setUserDetails(null);
-      setSerialNumber("");
-    } catch (err: any) {
-      console.log(err);
-      if (err.code === "ERR_NETWORK") {
-        setError("Error checking in! Try again later");
-        toast({
-          title: "Error.",
-          description: "Error checking in! Try again later",
-          status: "error",
-          duration: 3000,
-          isClosable: true,
-        });
-      } else {
-        const error =
-          err.response?.data?.message ?? "Error checking in! Try again later";
-        setError(error);
-        toast({
-          title: "Error.",
-          description: error,
-          status: "error",
-          duration: 3000,
-          isClosable: true,
-        });
-      }
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  async function clearArrears() {
-    try {
-      setLoading(true);
-      if (!token) {
-        toast({
-          title: "Error",
-          description: "Must be authenticated to update member details",
-          status: "error",
-          duration: 3000,
-          isClosable: true,
-        });
-        return;
-      }
-      const url = `/members/${userDetails.id}`;
-      const response = await updateArrears(url, token);
-      console.log(response);
-      setLoading(false);
-      setMessage("");
-      toast({
-        title: "Arrears Cleared",
-        description: `Successfully cleared ${userDetails.fullName}'s arrears`,
-        status: "success",
-        duration: 3000,
-        isClosable: true,
-      });
-      setUserDetails({ ...userDetails, arrears: 0 });
-    } catch (err: any) {
-      console.log(err);
-      if (err.code === "ERR_NETWORK") {
-        setError("Error clearing arrears! Try again later");
-        toast({
-          title: "Error.",
-          description: "Error clearing arrears! Try again later",
-          status: "error",
-          duration: 3000,
-          isClosable: true,
-        });
-      } else {
-        const error =
-          err.response?.data?.message ??
-          "Error clearing arrears! Try again later";
-        setError(error);
-        toast({
-          title: "Error.",
-          description: error,
-          status: "error",
-          duration: 3000,
-          isClosable: true,
-        });
-      }
-    } finally {
-      setLoading(false);
-    }
-  }
-
+function UserDetails({ userDetails }: { userDetails: UserStatusDetails }) {
   const { arrears } = userDetails;
-
   return (
     <div className="mt-[20px] pb-4">
       <TextContainer>
@@ -369,39 +187,6 @@ function UserDetails({
             {arrears}
           </Text>
         </TextContainer>
-      ) : null}
-      {arrears ? null : (
-        <Button
-          isLoading={loading}
-          colorScheme="primary"
-          variant="solid"
-          marginTop="20px"
-          width="100%"
-          backgroundColor="#179847"
-          onClick={checkIn}
-        >
-          Check In
-        </Button>
-      )}
-      {arrears ? (
-        <Button
-          isLoading={loading}
-          colorScheme="primary"
-          variant="solid"
-          marginTop="20px"
-          width="100%"
-          backgroundColor="#FACC00"
-          onClick={clearArrears}
-        >
-          Clear Arrears
-        </Button>
-      ) : null}
-      {message ? (
-        <Container centerContent marginTop="10px">
-          <Text color="green" as="b" paddingStart="5px" textAlign="center">
-            {message}
-          </Text>
-        </Container>
       ) : null}
     </div>
   );
